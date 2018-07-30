e3722edaa954c6153e7667ae66604a567b192fab
Rodrigo Gomensoro <rodrigo.gomensoro@gmail.com>
2018-07-30 20:37:46 -0300

    Heroku ready app

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+var port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -23,11 +24,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-
-    res.render('maintain.hbs');    
-
-});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -63,7 +59,7 @@ app.get('/bad', (req, res) => {
     });
 });
 
-    app.listen(3000, () => {
-        console.log('Server is up on port 3000.');
+    app.listen(port, () => {
+        console.log(`Server is up on port ${port}.`);
 
     });
